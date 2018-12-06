a9c7ee65a348210351b5127ce44c318ced679a3b
ultracake <jackievuong@live.dk>
2018-12-06 15:29:38 +0100

    noget lej med push to web hehe :)

--- src/main/java/com/administration/StudentAdministration/Controllers/RESTController.java
+++ src/main/java/com/administration/StudentAdministration/Controllers/RESTController.java
@@ -1,10 +1,26 @@
 package com.administration.StudentAdministration.Controllers;
 
+import com.administration.StudentAdministration.Models.CourseModels.CourseWebModel;
+import com.administration.StudentAdministration.Services.CourseServices.CourseRestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RESTController
 {
-    //public ResponseBody
+    @Autowired
+    private CourseRestService courseRestService;
+
+
+
+    @PostMapping("/push")
+    public String pushToWeb()
+    {
+        courseRestService.addCourseToLegacy();
+
+        return "Courses/course";
+    }
 }
--- src/main/java/com/administration/StudentAdministration/Services/CourseServices/CourseRestService.java
+++ src/main/java/com/administration/StudentAdministration/Services/CourseServices/CourseRestService.java
@@ -2,11 +2,13 @@ package com.administration.StudentAdministration.Services.CourseServices;
 
 import com.administration.StudentAdministration.Models.CourseModels.CourseModel;
 import com.administration.StudentAdministration.Models.CourseModels.CourseWebModel;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CourseRestService
 {
     public void addCourseToLegacy()
@@ -16,6 +18,9 @@ public class CourseRestService
 
         RestTemplate restTemplate = new RestTemplate();
 
-        //restTemplate.postForLocation(devURL, new CourseWebModel())
+        restTemplate.postForLocation(devURL, new CourseWebModel(0,1,"jackies test","","","","",true,1,""));
+
+
+
     }
 }
