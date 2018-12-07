5901daf1b1c8c66b5e0d05092d12f58536372c13
ultracake <jackievuong@live.dk>
2018-12-07 09:54:59 +0100

    consume all er på admin

--- src/main/java/com/administration/StudentAdministration/Controllers/AdminController.java
+++ src/main/java/com/administration/StudentAdministration/Controllers/AdminController.java
@@ -1,8 +1,12 @@
 package com.administration.StudentAdministration.Controllers;
 
 import com.administration.StudentAdministration.Services.AdminServices.AdminServiceImpl;
+import com.administration.StudentAdministration.Services.CourseServices.CourseServiceImpl;
+import com.administration.StudentAdministration.Services.StudentServices.StudentServiceImpl;
+import com.administration.StudentAdministration.Services.TeacherServices.TeacherServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,4 +15,21 @@ public class AdminController
 {
     @Autowired
     private AdminServiceImpl adminService;
+    @Autowired
+    StudentServiceImpl studentService;
+    @Autowired
+    private TeacherServiceImpl teacherService;
+    @Autowired
+    private CourseServiceImpl courseService;
+
+    @GetMapping("/consume")
+    public String consumeAll()
+    {
+        studentService.consumeWebService();
+        teacherService.consumeWebService();
+        courseService.consumeWebService();
+
+        return "redirect:/student/course";
+    }
+
 }
